a4230b86e1a08c866adef57590863bd330b73c03
Ihor Tarknaiev <13971793it@gmail.com>
2025-03-31 20:18:48 +0200

    override js

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,10 +1,12 @@
 import AjaxLoadPlugin from './example-plugin/example-plugin.plugin';
-import FirstPlugin from './first-plugin/first-plugin.plugin';
-
 window.PluginManager.register('AjaxLoadPlugin', AjaxLoadPlugin, '[data-ajax-helper]');
 
+
 //import
+// import FirstPlugin from './first-plugin/first-plugin.plugin';
 // window.PluginManager.register('FirstPlugin', FirstPlugin, '[test-div]');
 
 //async import
 window.PluginManager.register('FirstPlugin', () => import('./first-plugin/first-plugin.plugin'), '[test-div]');
+
+window.PluginManager.override('CookiePermission', () => import('./my-cookie-permission/my-cookie-permission.plugin'), '[data-cookie-permission]');
