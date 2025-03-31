8545f0736603d17272cf77b96b9fcd4e720ddd61
Ihor Tarknaiev <13971793it@gmail.com>
2025-03-31 19:36:37 +0200

    add custom js, biding to the DOM, register async, config plugin

---
+++ src/Resources/app/storefront/src/first-plugin/first-plugin.plugin.js
@@ -0,0 +1,17 @@
+const { PluginBaseClass } = window;
+
+export default class FirstPlugin extends PluginBaseClass {
+    static options = {
+        text: ' NEW NEW NEW Seems like there\'s nothing more to see here.',
+    };
+
+    init() {
+        window.addEventListener('scroll', this.onScroll.bind(this));
+    }
+
+    onScroll() {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            alert(this.options.text);
+        }
+    }
+}
--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,3 +1,10 @@
 import AjaxLoadPlugin from './example-plugin/example-plugin.plugin';
+import FirstPlugin from './first-plugin/first-plugin.plugin';
 
 window.PluginManager.register('AjaxLoadPlugin', AjaxLoadPlugin, '[data-ajax-helper]');
+
+//import
+// window.PluginManager.register('FirstPlugin', FirstPlugin, '[test-div]');
+
+//async import
+window.PluginManager.register('FirstPlugin', () => import('./first-plugin/first-plugin.plugin'), '[test-div]');
